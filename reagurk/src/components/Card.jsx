b6b8cfb1fef767657783a4195efa433d04f8337f
fisher60 <wakefollower@gmail.com>
2020-12-31 14:37:23 -0800

    add props and list style to cards

--- reagurk/src/components/Card.jsx
+++ reagurk/src/components/Card.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import "./Card.css";
+import benefitImg from "./images/parkingpage/checkmark.png";
 
-export function Card(){
+export function Card(props){
+    const benefitsList = props.benefits.map((benefit) => <li><img className="checkmark" src={benefitImg} alt="checkmark"/>{benefit}</li>);
     return (
         <div className="card">
-            <h1>Gurkan</h1>
-            <ul>
-                <li>Hello</li>
-                <li>World</li>
-            </ul>
-            <button className="joinButton">Sign Up Now</button>
+            <h1>{ props.title }</h1>
+            <ul>{benefitsList}</ul>
+            <button className={"cardButton " + (props.joinButton ? "join" : "learnMore")}>{props.joinButton ? "Sign Up Now" : "Learn More"}</button>
         </div>
     )
 }
--- reagurk/src/components/Cards.jsx
+++ reagurk/src/components/Cards.jsx
@@ -3,12 +3,15 @@ import {Card} from "./Card"
 import "./Cards.css";
 
 export function Cards(){
+    const nonGurkanBenefits = ["None", "pretty much nothing", "we will be sad"];
+    const gurkanBenefits = ["Community", "Contributing", "Knowledge"];
+    const gurkanLordBenefits = ["Power", "All-knowing", "Leadership"];
     return (
         <div className="cards">
             {/* <div className="card-bg"></div> */}
-            <Card />
-            <Card />
-            <Card />
+            <Card title="Non-Gurkan" benefits={nonGurkanBenefits}/>
+            <Card title="Gurkan" joinButton={true} benefits={gurkanBenefits}/>
+            <Card title="Lord Gurkan" benefits={gurkanLordBenefits}/>
         </div>
     )
 }
