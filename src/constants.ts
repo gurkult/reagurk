d07c7b438d5827e70f10ad9be911f2e8c5285287
DawnOfMidnight <dawnofmidnight1128@gmail.com>
2021-04-29 20:52:35 -0400

    Establish Base
    Set up the base file structure. Pages were left in `/src` directory because of React's limitations

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const serverInvite: string = "https://discord.gg/f6TH4rd5uw";
+export const shop: string = "https://gurkult.redbubble.com";
+export const github: string = "https://github.com/gurkult/";
