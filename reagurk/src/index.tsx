0049969aaa390d65ef5ce025a72aaaef56011644
Rishi Kumar Ray <87641376+RishiKumarRay@users.noreply.github.com>
2022-01-21 13:34:31 +0530

    updated changes

--- reagurk/src/index.tsx
+++ reagurk/src/index.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
 import './styles/index.css';
-import gurkultLogo from './assets/logo.png';
 
 ReactDOM.render(
   <React.StrictMode>
    
-    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
